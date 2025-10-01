05c372abdb803cdf8c2d70fa6e70b510e9cf6bcd
Felipe Guimarães <feliphe133bolado@gmail.com>
2025-09-30 21:18:59 -0300

    OrderItem, Many-to-many association with extra attributes PT1

---
+++ src/main/java/com/educandofe/course/Model/OrderItemModel.java
@@ -0,0 +1,88 @@
+package com.educandofe.course.Model;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.EmbeddedId;
+import java.io.Serializable;
+
+import com.educandofe.course.Model.pk.OrderItemPk;
+
+@Entity
+@Table(name = "tb_order_item")
+public class OrderItemModel implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @EmbeddedId
+    private OrderItemPk id;
+
+    private Integer quantity;
+    private Double price;
+
+    public OrderItemModel() {
+    }
+    
+    public OrderItemModel(OrderModel order, ProductModel product, Integer quantity, Double price) {
+        this.id.setOrder(order);
+        this.id.setProduct(product);
+        this.quantity = quantity;
+        this.price = price;
+    }
+    public Integer getQuantity() {
+        return quantity;
+    }
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+    public Double getPrice() {
+        return price;
+    }
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+    public OrderItemPk getId() {
+        return id;
+    }
+    public void setId(OrderItemPk id) {
+        this.id = id;
+    }
+    public ProductModel getProduct() {
+        return id.getProduct();
+    }
+    
+    public void setProduct(ProductModel product) {
+        id.setProduct(product);
+    }
+    
+    public OrderModel getOrder() {
+        return id.getOrder();
+    }
+    
+    public void setOrder(OrderModel order) {
+        id.setOrder(order);
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        OrderItemModel other = (OrderItemModel) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+}
---
+++ src/main/java/com/educandofe/course/Model/pk/OrderItemPk.java
@@ -0,0 +1,70 @@
+package com.educandofe.course.Model.pk;
+import java.io.Serializable;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+
+import com.educandofe.course.Model.OrderModel;
+import com.educandofe.course.Model.ProductModel;
+import jakarta.persistence.ManyToOne;
+
+@Embeddable
+public class OrderItemPk implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private OrderModel order;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private ProductModel product;
+
+
+    public OrderModel getOrder() {
+        return order;
+    }
+
+    public void setOrder(OrderModel order) {
+        this.order = order;
+    }
+
+    public ProductModel getProduct() {
+        return product;
+    }
+
+    public void setProduct(ProductModel product) {
+        this.product = product;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((order == null) ? 0 : order.hashCode());
+        result = prime * result + ((product == null) ? 0 : product.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        OrderItemPk other = (OrderItemPk) obj;
+        if (order == null) {
+            if (other.order != null)
+                return false;
+        } else if (!order.equals(other.order))
+            return false;
+        if (product == null) {
+            if (other.product != null)
+                return false;
+        } else if (!product.equals(other.product))
+            return false;
+        return true;
+    }
+    
+}
