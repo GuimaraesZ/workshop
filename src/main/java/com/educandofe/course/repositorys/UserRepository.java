139950a7b1e1ff631aae4c5b4518226f3478f139
Felipe Guimarães <feliphe133bolado@gmail.com>
2025-10-04 21:17:30 -0300

    feat: implement JWT authentication system with login and signup

--- src/main/java/com/educandofe/course/repositorys/UserRepository.java
+++ src/main/java/com/educandofe/course/repositorys/UserRepository.java
@@ -1,8 +1,17 @@
 package com.educandofe.course.repositorys;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.educandofe.course.Model.UserModel;
 
 public interface UserRepository  extends JpaRepository<UserModel, Long> {
+    
+    /**
+     * Busca um usuário por email
+     * @param email Email do usuário
+     * @return Optional com o usuário se encontrado
+     */
+    Optional<UserModel> findByEmail(String email);
     
 }
